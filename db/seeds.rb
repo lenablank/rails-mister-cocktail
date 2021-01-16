e897db9e7828bd360a10592f74cb35cadad9b787
Lena Blank <mine.blank@gmail.com>
2021-01-16 12:16:38 +0100

    added ingredient and dose model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "kahlua")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "bourbon")
+Ingredient.create(name: "rum")
+Ingredient.create(name: "strawberry")
+Ingredient.create(name: "whiskey")
